8fb91e042be7960734b29f5b5cf87d688576fe7b
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-11 13:16:31 -0500

    Improving styling, praise God

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,90 +1,57 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Grid, TextField } from "@mui/material";
+import "./Navbar.css"; // Importing the CSS file for styling
 
 function Navbar() {
+  const [chapterInput, setChapterInput] = useState("");
+  const [verseInput, setVerseInput] = useState("");
+
+  const handleChapterInputChange = (event) => {
+    const { value } = event.target;
+    setChapterInput(value.replace(/\D/, ""));
+  };
+
+  const handleVerseInputChange = (event) => {
+    const { value } = event.target;
+    setVerseInput(value.replace(/\D/, ""));
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            MUI
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" className="navbar">
+      <Toolbar className="toolbar">
+        <Typography variant="h6" component="div" className="title">
+          قرآن پارسی
+        </Typography>
+        <div className="search-fields">
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <TextField
+                id="chapter-search"
+                label="Search Chapter"
+                variant="outlined"
+                size="small"
+                placeholder="Enter chapter number"
+                value={chapterInput}
+                onChange={handleChapterInputChange}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="verse-search"
+                label="Search Verse"
+                variant="outlined"
+                size="small"
+                placeholder="Enter verse number"
+                value={verseInput}
+                onChange={handleVerseInputChange}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
