0354116243652397e8a6a1f5ab7ff7baa8a9ce49
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-11 21:40:21 -0500

    Added basmalah and button for searching, praise God

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, Grid, TextField } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+} from "@mui/material";
 import "./Navbar.css"; // Importing the CSS file for styling
 
 function Navbar() {
@@ -8,45 +15,66 @@ function Navbar() {
 
   const handleChapterInputChange = (event) => {
     const { value } = event.target;
-    setChapterInput(value.replace(/\D/, ""));
+    setChapterInput(value.replace(/[^0-9۰-۹]/g, "")); // Updated regex to include Arabic numerals
   };
 
   const handleVerseInputChange = (event) => {
     const { value } = event.target;
-    setVerseInput(value.replace(/\D/, ""));
+    setVerseInput(value.replace(/[^0-9۰-۹]/g, "")); // Updated regex to include Arabic numerals
   };
 
   return (
-    <AppBar position="static" className="navbar">
+    <AppBar
+      position="sticky"
+      className="navbar"
+      sx={{ backgroundColor: "#00a3cc" }}
+    >
       <Toolbar className="toolbar">
-        <Typography variant="h6" component="div" className="title">
+        <Typography
+          variant="h6"
+          component="div"
+          className="title"
+          sx={{ display: { xs: "none", sm: "none", md: "block", lg: "block" } }}
+        >
           قرآن پارسی
         </Typography>
+        <Typography
+          variant="h6"
+          component="div"
+          className="title"
+          sx={{ display: { xs: "none", sm: "none", md: "none", lg: "block" } }}
+        >
+          به نام خداوند بخشندترین، مهربانترین
+        </Typography>
         <div className="search-fields">
-          <Grid container spacing={2} alignItems="center">
+          <Grid container spacing={"1em"} alignItems="center">
             <Grid item>
               <TextField
                 id="chapter-search"
-                label="Search Chapter"
+                label="شماره سوره"
                 variant="outlined"
                 size="small"
-                placeholder="Enter chapter number"
+                placeholder="شماره سوره وارد کنید"
                 value={chapterInput}
                 onChange={handleChapterInputChange}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9۰-۹]*" }}
               />
             </Grid>
             <Grid item>
               <TextField
                 id="verse-search"
-                label="Search Verse"
+                label="شماره ایه"
                 variant="outlined"
                 size="small"
-                placeholder="Enter verse number"
+                placeholder="شماره ایه وارد کنید"
                 value={verseInput}
                 onChange={handleVerseInputChange}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9۰-۹]*" }}
               />
+            </Grid>
+            <Grid item>
+              {" "}
+              <Button variant="contained">جستوجو</Button>
             </Grid>
           </Grid>
         </div>
