f174c144abdd4b2ea23530dddfc0faa23107144a
saji2000 <sajaddaneshmand79@gmail.com>
2024-02-24 20:06:39 -0500

    Tried a new method of fetching data from json file but did not work

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -7,7 +7,34 @@ const Screen = () => {
   useEffect(() => {
     setVerses(data);
   }, []);
+
   const theme = useTheme();
+
+  // const [currentChapter, setCurrentChapter] = useState(1); // Start with chapter 1
+
+  // const fetchData = async (filePath) => {
+  //   try {
+  //     const response = await fetch(filePath);
+  //     if (!response.ok) {
+  //       throw new Error(`Error fetching data: ${response.status}`);
+  //     }
+  //     console.log("response:", response);
+
+  //     const data = await response.json();
+  //     console.log("data:", data);
+
+  //     return data;
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   const filePath = `../data/quran_chapters/chapter_${currentChapter}.json`;
+  //   fetchData(filePath)
+  //     .then((data) => setVerses(data))
+  //     .catch((error) => console.log(error));
+  // }, [currentChapter]);
 
   return (
     <Container
@@ -19,6 +46,7 @@ const Screen = () => {
         paddingBottom: theme.spacing(4),
       }}
     >
+      {console.log("verses:", verses)}
       {verses.length > 0 ? (
         verses.map((verse) => (
           <Box key={verse.verse_num} sx={{ marginBottom: theme.spacing(2) }}>
