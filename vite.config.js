9b3a0be2376daa9dfefd51fdf5c34143e3bbda89
saji2000 <sajaddaneshmand79@gmail.com>
2024-04-08 20:45:34 -0400

    Quran Parsi is now a PWA, praise God

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Optional: how service worker is registered
+      workbox: {
+        // Workbox options (see Workbox docs for details)
+      },
+      manifest: {
+        name: "Quran Parsi",
+        short_name: "Quran",
+        description: "Quran's translation in Persian",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./src/assets/react.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          // ... add other icon sizes
+        ],
+      },
+    }),
+  ],
+});
